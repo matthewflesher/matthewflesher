937d4bd4bd3f5dfd8e04a8919b250d70d95fc55c
Matthew Flesher <atlanticcitytc2@gmail.com>
2025-05-04 15:15:11 -0400

    updated deploy cicd

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: process.env.CYPRESS_BASE_URL || "http://localhost:3000",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // you can access env variables here too if needed
+      return config;
     },
   },
 });
+
---
+++ cypress/e2e/smoke.cy.js
@@ -0,0 +1,11 @@
+describe('Email is displayed', () => {
+    beforeEach(() => {
+      cy.visit('/');
+    });
+  
+    it('should show the correct email address in the footer', () => {
+      cy.fixture('example').then((data) => {
+        cy.contains(data.email).should('be.visible');
+      });
+    });
+  });
